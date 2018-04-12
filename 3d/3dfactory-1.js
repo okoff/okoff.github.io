3d7894cca395f3fd842a542fe21e213d2a88dd52
okoff <okoff@yandex.ru>
2018-04-12 20:17:16 +0300

    Треугольники

--- 3d/3dfactory-1.js
+++ 3d/3dfactory-1.js
@@ -8,10 +8,36 @@ class Factory3D {
 		let obj = {};
 		obj.vertexes = vertexes;
 		obj.edges = edges;
-		obj.triangles = [];
 		this.objects.push(obj);
-		console.log("Objects: " + this.objects.length);
+//		console.log("Objects: " + this.objects.length);
 		return obj;
+	}
+	
+	triangles (obj) {
+		obj.triangles = [];
+		for (let v1=0; v1<obj.edges.length-1; v1++) {
+			for (let v2=v1+1; v2<obj.edges.length; v2++) {
+				if(obj.edges[v1].a == obj.edges[v2].a) {
+					let t = {
+						a: obj.edges[v1].a,
+						b: obj.edges[v1].b,
+						c: obj.edges[v2].b
+					};
+					obj.triangles.push(t);
+					console.log("+t(a): " + t.a + " " + t.b + " " + t.c);
+				}
+				if(obj.edges[v1].b == obj.edges[v2].a) {
+					let t = {
+						a: obj.edges[v1].a,
+						b: obj.edges[v1].b,
+						c: obj.edges[v2].b
+					};
+					obj.triangles.push(t);
+					console.log("+t(b): " + t.a + " " + t.b + " " + t.c);
+				}
+			}
+		}
+		return obj.triangles;
 	}
 
 }
